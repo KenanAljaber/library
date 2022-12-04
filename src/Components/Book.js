3a0ad8269f838ac2d906f296443c49568c0a30f5
kenoisplaying <keno12333@hotmail.com>
2022-12-04 12:23:16 -0500

    changing colors

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import "../Styles/book.css"
 const Book = ({title,cover,author,id}) => {
+    
     return ( 
         <div className="bookContainer">
-            <div className="bookTitle">{title}</div>
+            <div className="bookTitle">{title.length>15 ? title.substring(0,15):title}</div>
             <div className="bookAuthor">Written by: {author}</div>
             <img src={cover}/>
             <Link to={`/book/${id}`} > <button className="details" >Details</button></Link>
