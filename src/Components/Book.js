17fdc747aeae465bfa1db0b9ace174bf09d1281c
kenoisplaying <keno12333@hotmail.com>
2022-12-05 15:04:25 -0500

    fixing responsiveness issue

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -5,7 +5,7 @@ const Book = ({title,cover,author,id}) => {
     return ( 
         <div className="bookContainer">
             <div className="bookTitle">{title.length>15 ? title.substring(0,15):title}</div>
-            <div className="bookAuthor">Written by: {author}</div>
+            <div className="bookAuthor">Written by: {author? author : "Uknown"}</div>
             <img src={cover}/>
             <Link to={`/book/${id}`} > <button className="details" >Details</button></Link>
 
